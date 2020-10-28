bd51df3612a8f5d3f06387405f313bc6d229733e
sofiadavidov <sofi483@gmail.com>
2020-10-28 09:20:00 +0200

    whos-there get request

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 //init app
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => res.send('Homepage'));
+app.get('/whos-there', (req, res) => res.send('Hi Trax! This is Sofia'));
 
 //start the server
 app.listen(PORT, () =>
